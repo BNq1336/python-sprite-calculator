2f09c2e5cf2e06e85d8beb759fa8170c1b6c9e50
pawelbaginski <140252039+BNq1336@users.noreply.github.com>
2026-05-11 14:16:00 +0200

    Add files via upload

---
+++ Calculator/Calculator/Calculator.py
@@ -0,0 +1,107 @@
+from sqlite3.dbapi2 import Timestamp
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((480,700))
+pygame.display.set_caption("Helluva calculator")
+
+RED = (216,41,50)
+BLACK = (0,0,0)
+WHITE = (254,255,250)
+
+calculation = ""
+operators = "+-/*%"
+
+font = pygame.font.SysFont(None, 50)
+
+class Button:
+   
+    def __init__(self, screen, color, x, y, width, height, text):
+        self.color = color
+        self.text = text
+        self.rect = pygame.Rect(x, y, width, height)
+    
+    def button_draw(self):
+        pygame.draw.rect(screen, self.color, self.rect, border_radius=15)
+        
+        button_inside = font.render(self.text, True, (254,255,250))
+        button_inside_rect = button_inside.get_rect(center=self.rect.center)
+
+        screen.blit(button_inside, button_inside_rect)
+
+    def hover(self):
+        mouse_pos = pygame.mouse.get_pos()
+
+        if self.rect.collidepoint(mouse_pos):
+            self.color = (44,42,54)
+        else:
+            self.color = (0,0,0)
+
+    def click(self, event, calculation, operators):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.rect.collidepoint(event.pos):
+                try:
+                    if self.text == "C":
+                        calculation = ""                 
+                    elif self.text == "=":
+                        calculation = str(eval(calculation))
+                    else:
+                        calculation += self.text
+                except:
+                    calculation = "Error"
+        return calculation
+                
+button_data = [
+    ("1", 50, 500, 80, 80),
+    ("2", 150, 500, 80, 80),
+    ("3", 250, 500, 80, 80),
+    ("4", 50, 400, 80, 80),
+    ("5", 150, 400, 80, 80),
+    ("6", 250, 400, 80, 80),
+    ("7", 50, 300, 80, 80),
+    ("8", 150, 300, 80, 80),
+    ("9", 250, 300, 80, 80),
+    ("0", 150, 600, 80, 80),
+    ("+", 350, 500, 80, 80),
+    ("-", 350, 400, 80, 80),
+    ("/", 350, 300, 80, 80),
+    ("*", 350, 200, 80, 80),
+    ("%", 250, 200, 80, 80),
+    ("=", 250, 600, 180, 80),
+    ("C", 50, 200, 180, 80),
+    (".", 50, 600, 80, 80)
+]
+
+buttons = []
+
+for text, x, y, width, height in button_data:
+    button = Button(screen, BLACK, x, y, width, height, text)
+    buttons.append(button)
+
+running = True
+
+while running:
+
+    for button in buttons:
+        button.hover()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        
+        for button in buttons:
+            calculation = button.click(event, calculation, operators)
+
+    screen.fill(RED)
+
+    pygame.draw.rect(screen, WHITE,(50, 50, 380, 100), border_radius=10)
+    calculation_surface = font.render(calculation, True, BLACK)
+    screen.blit(calculation_surface, (80, 85))
+
+    for button in buttons:
+        button.button_draw()
+
+    pygame.display.update()
+
+pygame.quit()
